c0c7e2df2932d73f22eae56adf5b1deb50cda2c3
Jiahe Chen <psyjc3@exmail.nottingham.ac.uk>
2018-01-14 17:09:49 -0500

    Designed the (guess and random part) (Next steps: routing)

--- src/Layout/Choices/Card/Card.js
+++ src/Layout/Choices/Card/Card.js
@@ -5,40 +5,70 @@ import fruitIcon from '../../../img/fruit.png';
 import drinkIcon from '../../../img/drink.png';
 import snackIcon from '../../../img/snack.png';
 class Card extends Component {
+    guessHandler = () => {
+        console.log("I am guessing");
+    }
+    randomHandler = () => {
+        console.log("I am randoming");
+    }
     render(){
+        const detailedBox = {display:'none', height:'60px'};
         let cardColor = null;
         let cardIcon = null;
+        let cardButton = {position: 'fixed', right:'20'};
+        let cardTitle = null;
         switch(this.props.type){
             case('Staple Food'):
                 cardColor = {backgroundColor: '#e4c561',color:"#c98c47"};
                 cardIcon = foodIcon;
+                cardTitle = '主食';
                 break;
             case('Fruit'):
                 cardColor = {backgroundColor:'#dc7254', color: "#9b3731"};
                 cardIcon = fruitIcon;
+                cardTitle = '水果';
                 break;
             case('Drink'):
                 cardColor = {backgroundColor:'#98394f', color: "#611533"};
                 cardIcon = drinkIcon;
+                cardTitle = '饮料';
                 break;
             case('Snack'):
                 cardColor = {backgroundColor:'#4f2f55', color: "#251334"};
                 cardIcon = snackIcon;
+                cardTitle = '零食';
                 break;
             default:
                 cardColor = {backgroundColor:'#000000'};
                 break;
         }
+        let clickCard = () => {
+            if(document.getElementById(this.props.type).style.display === "none"){
+                document.getElementById('Staple Food').style.display = "none";
+                document.getElementById('Fruit').style.display = "none";
+                document.getElementById('Drink').style.display = "none";
+                document.getElementById('Snack').style.display = "none";
+                document.getElementById(this.props.type).style.display = "block";
+                
+            }
+            else{
+                document.getElementById(this.props.type).style.display = "none";
+            }
+        };
         return(
-            <div className={classes.ChoiceCard} style={cardColor}>
+            <div className={classes.ChoiceCard} style={cardColor} onClick={clickCard}>
                 <div className="container">
                     <div className="row">
-                        <div className="col-3">
+                        <div className="col-4">
                             <img src={cardIcon} className={classes.cardIcon}/>
                         </div>
                         <div className="col-8">
-                            <p className={classes.CardP}>{this.props.type}</p>
+                            <p className={classes.CardP}>{cardTitle}</p>
                         </div>
+                    </div>
+                    <div id={this.props.type} style={detailedBox}>
+                        <button type="button" className="btn btn-outline-light" onClick={this.guessHandler}>让派派猜猜:D</button> 
+                        <button type="button" className="btn btn-outline-light" onClick={this.randomHandler} style={cardButton} style={{position:'absolute', right:10}}>让派派随机:D</button>
                     </div>
                 </div>
             </div>
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,11 +2,22 @@ import React from 'react';
 import classes from './Layout.css';
 
 const layout = (props) => {
-    let msg = ["Good Morning", "Good Afternoon", "Good Evening"];
+    var d = new Date();
+    var h = d.getHours();
+    var msg = "";
+    if(h < 12){
+        msg = "早上好(◍•ᴗ•`◍)❤年年";
+    }
+    else if(h >= 17){
+        msg = "晚上好(◍•ᴗ•`◍)❤年年";
+    }
+    else{
+        msg = "下午好(◍•ᴗ•`◍)❤年年";
+    }
     return(
         <div className={classes.parentCla}>
             <nav className={classes.NavBar}>
-                <p>{msg[0]}</p>
+                <p>{msg}<br/>派派来解决吃饭问题了٩٩꒰｡•◡•｡꒱۶</p>
             </nav>
             <main>
                 {props.children}
